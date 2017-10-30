9870abd9f7f713ed3272939eed19dab2a964bdcb
Geoff Cox <redgeoff@gmail.com>
2017-10-30 16:59:26 -0700

    test(integration): change listening

--- test/integration/index.js
+++ test/integration/index.js
@@ -20,28 +20,6 @@ describe('integration', () => {
     await testUtils.createDB('test_db2' + suffix)
   }
 
-  beforeEach(async () => {
-    suffix = testUtils.nextSuffix()
-
-    server = new Server()
-    await server.start()
-
-    spawner = new Spawner()
-    await spawner.start()
-
-    await createReplicator()
-
-    await createTestDBs()
-  })
-
-  afterEach(async () => {
-    await spawner.stop()
-
-    await server.stop()
-
-    await testUtils.destroyTestDBs()
-  })
-
   const createReplicator = async () => {
     // URL w/o the password
     let url =
@@ -60,6 +38,38 @@ describe('integration', () => {
     })
   }
 
+  const createOnChange = async () => {
+    await spawner._spiegel._slouch.doc.create(spawner._spiegel._dbName, {
+      type: 'on_change',
+      db_name: 'test_db1' + suffix,
+      url: 'http://user@localhost:3000/foo'
+    })
+  }
+
+  beforeEach(async () => {
+    suffix = testUtils.nextSuffix()
+
+    server = new Server()
+    await server.start()
+
+    spawner = new Spawner()
+    await spawner.start()
+
+    await createReplicator()
+
+    await createOnChange()
+
+    await createTestDBs()
+  })
+
+  afterEach(async () => {
+    await spawner.stop()
+
+    await server.stop()
+
+    await testUtils.destroyTestDBs()
+  })
+
   it('should replicate and listen to changes', async () => {
     // TODO: use testUtils.waitFor instead of timeout
     await sporks.timeout(18000)
@@ -72,5 +82,9 @@ describe('integration', () => {
       include_docs: true
     })
     docs2.should.eql(docs1)
+
+    // Make sure the API was called during the change listening. Depending on the timing, it
+    // possible to receive multiple requests.
+    testUtils.shouldEqual(server.numRequests > 0, true)
   })
 })
