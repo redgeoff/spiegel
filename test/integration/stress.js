ddd336d812416430217cb7c0690a681999885d68
Geoff Cox <redgeoff@gmail.com>
2018-01-14 07:42:35 -0800

    test(stress): single listener for all trials

--- test/integration/stress.js
+++ test/integration/stress.js
@@ -6,10 +6,11 @@ const utils = require('../../src/utils')
 const sporks = require('sporks')
 
 describe('stress', function() {
-  const NUM_USERS = 3
-  const NUM_MESSAGES = 1000
+  const NUM_USERS = 5
+  const NUM_MESSAGES = 3
+  const TIMEOUT = 3000000
 
-  this.timeout(120000)
+  this.timeout(TIMEOUT)
 
   let spawner = null
   let userDBs = []
@@ -17,11 +18,16 @@ describe('stress', function() {
   let totalMs = null
   let received = null
   let userCount = null
+  let n = 0
+  let iterator = null
 
   const createUserDB = async() => {
     let dbName = 'user_' + userCount++
-    await testUtils._slouch.db.create(dbName)
+
+    // Have to push before creating to ensure sequential order in array
     userDBs.push(dbName)
+
+    await testUtils._slouch.db.create(dbName)
   }
 
   const createUserDBs = async() => {
@@ -68,15 +74,40 @@ describe('stress', function() {
   //   await Promise.all(replicatorIds.map(async id => await downsert(spawner._dbName, id)))
   // }
 
+  // In order to avoid missing changes due to race conditions when setting up a listener per trial
+  // we set up a single listener that will run for the duration of all our trials.
+  const listenForMessages = async() => {
+    iterator = testUtils._slouch.db.changes('_global_changes', {
+      feed: 'continuous',
+      heartbeat: true,
+      since: 'now'
+    })
+
+    received = {}
+
+    iterator.each(item => {
+      let change = item.id.split(':')
+
+      // Ignore any changes caused from creating the DB and not for the user DBs
+      if (change[0] === 'updated' && change[1].indexOf('user_') !== -1) {
+        received[change[1]] = true
+
+        // console.log('received=', received)
+      }
+    })
+  }
+
   before(async() => {
     replicatorIds = []
     spawner = new Spawner()
+    listenForMessages()
     await spawner.start()
     await createUserDBs()
     await createReplicators()
   })
 
   after(async() => {
+    iterator.abort()
     await spawner.stop()
     // await destroyReplicators()
     await destroyUserDBs()
@@ -87,49 +118,49 @@ describe('stress', function() {
     userCount = 1
   })
 
+  const numDocsFactory = j => {
+    return async() => {
+      let user = await testUtils._slouch.db.get('user_' + j)
+      if (user.doc_count !== j) {
+        console.error('doc_count !=', j, 'user=', user)
+      }
+    }
+  }
+
+  const checkNumDocs = async() => {
+    let promises = []
+    for (let j = 0; j < NUM_USERS; j++) {
+      promises.push(numDocsFactory(j))
+    }
+    await Promise.all(promises)
+  }
+
   afterEach(async() => {
+    await checkNumDocs()
+
     if (sporks.length(received) !== NUM_USERS) {
       console.error('received=', received)
       console.error('userDBs=', userDBs)
+      console.error('time is ', new Date())
       // console.error('num received', sporks.length(received))
-      // console.log('EXITING...')
-      // process.exit(-1)
+      console.log('EXITING...')
+      process.exit(-1)
     }
   })
 
   const waitForMessages = async() => {
-    let iterator = testUtils._slouch.db.changes('_global_changes', {
-      feed: 'continuous',
-      heartbeat: true,
-      since: 'now'
-    })
-
     received = {}
 
-    await iterator.each(item => {
-      let change = item.id.split(':')
-
-      // Ignore any changes caused from creating the DB and not for the user DBs
-      if (change[0] === 'updated' && change[1].indexOf('user_') !== -1) {
-        received[change[1]] = true
-
-        console.log('received=', received)
-
-        // Received changes for all DBs? The change to the first DB is done directly and the reset
-        // should receive replicated changes
-        if (sporks.length(received) === NUM_USERS) {
-          // console.log('received=', received)
-          iterator.abort()
-        }
-      }
-    })
+    await sporks.waitFor(() => {
+      return sporks.length(received) === NUM_USERS ? true : undefined
+    }, TIMEOUT)
   }
 
-  const sendMessage = async i => {
+  const sendMessage = async() => {
     let before = new Date()
     let waitFor = waitForMessages()
     let msg = {
-      msg: 'this is sample message ' + i,
+      msg: 'this is sample message ' + n,
       created_at: new Date().toISOString()
     }
     console.log('sending msg', msg)
@@ -142,7 +173,13 @@ describe('stress', function() {
 
   for (let i = 1; i <= NUM_MESSAGES; i++) {
     it('trial ' + i + ': replicate messages to ' + (NUM_USERS - 1) + ' users', async() => {
-      await sendMessage(i)
+      n = i
+
+      try {
+        await sendMessage()
+      } catch (err) {
+        console.log('ERROR FROM sendMessage, err=', err)
+      }
 
       if (i === NUM_MESSAGES) {
         console.log('Took an average of %s ms to send a message', totalMs / NUM_MESSAGES)
